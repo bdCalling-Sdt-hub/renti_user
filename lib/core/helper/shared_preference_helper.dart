7829240423db138e9e9b6667bdef66ffd51117fe
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2023-09-16 18:44:16 +0600

    feat(api service): done

---
+++ lib/core/helper/shared_preference_helper.dart
@@ -0,0 +1,22 @@
+class SharedPreferenceHelper{
+
+  static const String accessTokenKey = 'access_token';
+  static const String accessTokenType = 'access_type';
+  static const String resetPassTokenKey = 'reset_pass_token';
+  static const String userEmailKey = 'user_email';
+  static const String userNameKey = 'user_name';
+  static const String userPhoneNumberKey = 'user_phone_number';
+  static const String rememberMeKey = 'remember me';
+  static const String fcmDeviceKey='device-key';
+
+  static const String userIdKey = 'user_id';
+  static const String hasNewNotificationKey = 'new-notification-key';
+
+  static const String token = 'token';
+  static const String countryCode = 'country_code';
+  static const String languageCode = 'language_code';
+
+  static const String languageJson = 'language_json';
+  static const String languageKey = 'language-key';
+  static const String languageListKey = 'language-list-key';
+}
--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -5,121 +5,116 @@ import 'package:renti_user/core/global/api_response_method.dart';
 import 'package:http/http.dart' as http;
 import 'package:renti_user/core/global/api_authorization_response_model.dart';
 import 'package:renti_user/core/global/api_response_model.dart';
+import 'package:renti_user/core/helper/shared_preference_helper.dart';
 import 'package:renti_user/core/route/app_route.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiClient extends GetxService{
 
+  SharedPreferences sharedPreferences;
+  ApiClient({required this.sharedPreferences});
 
-  ApiClient();
-  
-  Future<ApiResponseModel> responseRequest(
-    String uri, 
-    String responseMethod,
-    Map<String, dynamic>? params, 
-    {bool passHeader = false, bool isOnlyAcceptType = false}) async{
-    
-    Uri url = Uri.parse(uri);
+  Future<ApiResponseModel> request(
+      String uri,
+      String method,
+      Map<String, dynamic>? params,
+      {bool passHeader=false,
+        bool isOnlyAcceptType=false,}) async {
+
+    Uri url=Uri.parse(uri);
     http.Response response;
 
     try {
-      if (responseMethod == ApiResponseMethod.postMethod) {
-        if(passHeader){
-          //initToken();
-          if(isOnlyAcceptType){
-            response = await http.post(url, body: params,headers: {
-              "Accept": "application/json",
-            });
-          }
-          else{
-            response = await http.post(url, body: params,headers: {
-              "Content-Type": "application/json",
-              "Authorization": "$tokenType $token"
-            });
-          }
-        }
-        else{
-          response = await http.post(url, body: params);
-        }
 
-      }
-      else if (responseMethod == ApiResponseMethod.postMethod) {
+      if (method == ApiResponseMethod.postMethod) {
         if(passHeader){
-          // initToken();
+          initToken();
           response = await http.post(
-            url,
-            body: params,
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": "$tokenType $token"
+              url,
+              body: params,
+              headers: {
+                "Accept": "application/json",
+                "Authorization": "$tokenType $token"
           });
         }
         else{
           response = await http.post(
-              url,
-              body: params
+            url,
+            body: params
           );
         }
       }
-      else if (responseMethod == ApiResponseMethod.deleteMethod) {
+      if (method == ApiResponseMethod.deleteMethod) {
         response = await http.delete(url);
-      } else if (responseMethod == ApiResponseMethod.updateMethod) {
+      }
+      if (method == ApiResponseMethod.updateMethod) {
         response = await http.patch(url);
-      } else {
+      }
+      else {
+
         if(passHeader){
-          //initToken();
+          initToken();
           response = await http.get(
-            url, headers: {
-            "Content-Type": "application/json",
+              url,headers: {
+            "Accept": "application/json",
             "Authorization": "$tokenType $token"
           });
+
         }else{
           response = await http.get(
             url,
           );
         }
       }
+
+      print(url.toString());
+      print(response.body);
+
+
       if (response.statusCode == 200) {
         try{
-          ApiAuthorizationResponseModel model = ApiAuthorizationResponseModel.fromJson(jsonDecode(response.body));
-          if(model.remark == 'unauthenticated') {
+          ApiAuthorizationResponseModel authorizationResponseModel = ApiAuthorizationResponseModel.fromJson(jsonDecode(response.body));
+          if(authorizationResponseModel.message == 'Unauthenticated' ){
+            sharedPreferences.setBool(SharedPreferenceHelper.rememberMeKey, false);
+            sharedPreferences.remove(SharedPreferenceHelper.token);
             Get.offAllNamed(AppRoute.signInScreen);
           }
         }catch(e){
           e.toString();
         }
-        return ApiResponseModel(200, "Success", response.body);
+
+        return ApiResponseModel(200, 'Success', response.body);
       }
       else if (response.statusCode == 401) {
-        Get.offAllNamed(AppRoute.signInScreen);
-        return ApiResponseModel(401, "Unauthorized", response.body);
+        sharedPreferences.setBool(SharedPreferenceHelper.rememberMeKey, false);
+        Get.offAllNamed(AppRoute. signInScreen);
+        return ApiResponseModel(401, "Unauthorized".tr, response.body);
       } else if (response.statusCode == 500) {
-        return ApiResponseModel(500, "Server Error", response.body);
+        return ApiResponseModel(500, "Internal Server Error".tr, response.body);
       } else {
-        return ApiResponseModel(499, "Something went wrong", response.body);
+        return ApiResponseModel(499, "Something went wrong".tr, response.body);
       }
     } on SocketException {
-      return ApiResponseModel(503, "No internet connection", '');
+      return ApiResponseModel(503, "No internet connection".tr, '');
     } on FormatException {
-      return ApiResponseModel(400, "Bad Response", '');
+      return ApiResponseModel(400, "Bad Response Request".tr, '');
     } catch (e) {
-      return ApiResponseModel(499, "Something went wrong", '');
+      return ApiResponseModel(499, "Client Closed Request".tr, '');
     }
   }
 
-  String token='';
-  String tokenType='';
+  String token = '';
+  String tokenType = '';
 
-  /*initToken() {
+  initToken() {
     if (sharedPreferences.containsKey(SharedPreferenceHelper.accessTokenKey)) {
-      String? t =
-      sharedPreferences.getString(SharedPreferenceHelper.accessTokenKey);
-      String? tType =
-      sharedPreferences.getString(SharedPreferenceHelper.accessTokenType);
+      String? t = sharedPreferences.getString(SharedPreferenceHelper.accessTokenKey);
+      String? tType = sharedPreferences.getString(SharedPreferenceHelper.accessTokenType);
       token = t ?? '';
       tokenType = tType ?? 'Bearer';
     } else {
       token = '';
       tokenType = 'Bearer';
     }
-  }*/
+  }
 }
